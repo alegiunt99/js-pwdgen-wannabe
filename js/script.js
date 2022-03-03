4599c058ed1247e81cd6ecb833ea38d9712cc2f2
Alessandro Giunta <alegiunt99@gmail.com>
2022-03-03 14:15:25 +0100

    Ho aggiunto i Prompt al per nome, cognome e colore

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Nome
+
+const nameUser = prompt()
+
+console.log(nameUser)
+
+
+// Cognome
+
+const surnameUser = prompt()
+
+console.log(surnameUser)
+
+
+
+// Colore preferito
+
+const favouriteColorUser = prompt()
+
+console.log(favouriteColorUser)
+
+
+
+
